0c327625fd4639fd4d52940d63dead446a3e0f26
Alex Jensen <123037284+ajensen32@users.noreply.github.com>
2024-02-06 15:44:12 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const empty = "";
+const uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%^&*=-_";
+
+const pLength = document.getElementById("p-length");
+const upperCase = document.getElementById("p-uppercase");
+const lowerCase = document.getElementById("p-lowercase");
+const pNumber = document.getElementById("p-number");
+const pSymbol = document.getElementById("p-symbol");
+const submit = document.getElementById("submit");
+const password = document.getElementById("password");
+
+submit.addEventListener("click", function(){
+    var initialPassword = empty;
+    (upperCase.checked) ? initialPassword += uCase : "";
+    (lowerCase.checked) ? initialPassword += lCase : "";
+    (pNumber.checked) ? initialPassword += number : "";
+    (pSymbol.checked) ? initialPassword += symbol : "";
+
+    password.value = generatePassword(pLength.value, 
+    initialPassword)
+});
+
+function generatePassword(l, initialPassword){
+    var pass = "";
+    for(var i = 0; i < l; i++){
+        pass += initialPassword.charAt(Math.floor(Math.random()
+        * initialPassword.length));
+    }
+    return pass;
+}
+
+
